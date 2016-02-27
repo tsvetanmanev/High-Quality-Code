a4974b3528b63466a5518a6434a3c429e1980c21
Tsvetan Manev <tsvetanmanev@gmail.com>
2016-02-27 20:19:21 +0200

    First commit of this

---
+++ 04. Code Documentation and Comments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Code_Documentation_and_Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hey there
+        }
+    }
+}
