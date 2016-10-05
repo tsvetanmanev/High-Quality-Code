d349b7720f767103747db0bc34cfa1a8139b686c
Tsvetan Manev <tsvetanmanev@gmail.com>
2016-10-05 21:36:30 +0300

    Better naming of Project and classes

--- 17. Creational design patterns/ConsoleApplication1/Program.cs
+++ 17. Creational design patterns/ConsoleApplication1/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace FactoryReporter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Create new Reporter Factory");
         }
     }
 }
